591db428f01e5d5d74b472481fac8bb38d01c639
Jonny Noone <jonny.f.noone@gmail.com>
2022-11-17 21:24:40 +0000

    exceptions

---
+++ 16_exceptions.php
@@ -0,0 +1,28 @@
+<?php
+
+function inverse($x) {
+    if (!$x) {
+        throw new Exception('Division by Zero');
+    }
+
+    return 1/$x;
+}
+
+try {
+    echo inverse(5);
+    echo inverse(0);
+} catch(Exception $e) {
+    echo 'Caught Exception: ', $e->getMessage();
+} finally {
+    echo 'First Finally';
+}
+
+try {
+    echo inverse(0);
+} catch(Exception $e) {
+    echo 'Caught Exception: ', $e->getMessage();
+} finally {
+    echo 'Second Finally';
+}
+
+echo 'Hello, world.';
